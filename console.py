a0f73832129e7beb394601bd61ebc06f123ba11a
Ayanda Ngubane <ayanda4me@gmail.com>
2023-12-09 04:01:14 -0800

    Console

--- console.py
+++ console.py
@@ -1,196 +1,226 @@
 #!/usr/bin/python3
-""" Holberton AirBnB console module """
+"""
+Command interpreter
+"""
+
 import cmd
-import sys
 import models
 from models.base_model import BaseModel
+from datetime import datetime
+import re
+import shlex
 from models.user import User
-from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
+from models.place import Place
 from models.review import Review
-import shlex
 
-classes = {'BaseModel': BaseModel, "User": User, "State": State, "City": City,
+my_classes = {"BaseModel": BaseModel, "User": User, "State": State, "City": City,
            "Amenity": Amenity, "Place": Place, "Review": Review}
 
 
 class HBNBCommand(cmd.Cmd):
-    """ HBNB class """
-    prompt = '(hbnb) '
+    """
+    Command interpreter
+    """
 
-    def do_quit(self, arg):
-        """ exit the program """
-        exit()
+    prompt = "(hbnb) "
 
-    def do_EOF(self, arg):
-        """ exit the program """
-        print('')
-        exit()
+    def do_quit(self, line):
+        """
+        to exit the program
+        """
+
+        return True
+
+    def do_EOF(self, line):
+        """
+        to exit the program
+        """
+
+        return True
 
     def emptyline(self):
-        """ shouldn't execute anthing """
+        """
+        When an empty line is entered in response to the prompt,
+        it won't repeat the last nonempty command entered.
+        """
+
         pass
 
-    def do_create(self, arg):
+    def do_create(self, line):
         """
-         Creates a new instance of BaseModel,
-         saves it (to the JSON file) and prints the id
-         """
-        cmd_args = shlex.split(arg)
-        if len(cmd_args) == 0:
-            print("** class name missing **")
-            return False
-        if cmd_args[0] in classes:
-            new_instance = classes[cmd_args[0]]()
-        else:
-            print("** class doesn't exist **")
-            return False
-        print(new_instance.id)
-        new_instance.save()
+        Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id. Ex: $ create BaseModel
+        If the class name is missing,
+        print ** class name missing ** (ex: $ create)
+        If the class name doesn’t exist,
+        print ** class doesn't exist ** (ex: $ create MyModel)
+        """
 
-    def do_show(self, arg):
-        """
-        Prints the string representation of an
-        instance based on the class name and id
-        """
-        cmd_args = shlex.split(arg)
-        if len(cmd_args) == 0:
+        c_name = self.parseline(line)[0]
+
+        if c_name == "" or c_name is None:
             print("** class name missing **")
-            return False
-        if cmd_args[0] in classes:
-            if len(cmd_args) > 1:
-                key = cmd_args[0] + "." + cmd_args[1]
-                if key in models.storage.all():
-                    print(models.storage.all()[key])
-                else:
-                    print("** no instance found **")
+
+        elif c_name not in my_classes:
+            print("** class doesn't exist **")
+
+        else:
+            new_instance = my_classes[c_name]()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, line):
+        """
+        Prints the string representation of an instance based on the class
+        name and id. Ex: $ show BaseModel 1234-1234-1234.
+        If the class name is missing,
+        print ** class name missing ** (ex: $ show)
+        If the class name doesn’t exist,
+        print ** class doesn't exist ** (ex: $ show MyModel)
+        If the id is missing,
+        print ** instance id missing ** (ex: $ show BaseModel)
+        If the instance of the class name doesn’t exist for the id,
+        print ** no instance found ** (ex: $ show BaseModel 121212)
+        """
+
+        c_name = self.parseline(line)[0]
+        i_id = self.parseline(line)[1]
+
+        if c_name == "" or c_name is None:
+            print("** class name missing **")
+
+        elif c_name not in my_classes:
+            print("** class doesn't exist **")
+
+        elif i_id == "" or i_id is None:
+            print('** instance id missing **')
+
+        else:
+            key = c_name + "." + i_id
+
+            if key in models.storage.all():
+                print(models.storage.all()[key])
             else:
-                print("** instance id missing **")
-        else:
-            print("** class doesn't exist **")
+                print("** no instance found **")
 
-    def do_destroy(self, arg):
+    def do_destroy(self, line):
         """
-         Deletes an instance based on the class name
-         and id (save the change into the JSON file
-         """
-        cmd_args = shlex.split(arg)
-        if len(cmd_args) == 0:
+        Deletes an instance based on the class name and
+        id (save the change into the JSON file).
+        Ex: $ destroy BaseModel 1234-1234-1234.
+        If the class name is missing,
+        print ** class name missing ** (ex: $ destroy)
+        If the class name doesn’t exist,
+        print ** class doesn't exist ** (ex:$ destroy MyModel)
+        If the id is missing,
+        print ** instance id missing ** (ex: $ destroy BaseModel)
+        If the instance of the class name doesn’t exist for the id,
+        print ** no instance found ** (ex: $ destroy BaseModel 121212)
+        """
+
+        c_name = self.parseline(line)[0]
+        i_id = self.parseline(line)[1]
+
+        if c_name == "" or c_name is None:
             print("** class name missing **")
-        elif cmd_args[0] in classes:
-            if len(cmd_args) > 1:
-                key = cmd_args[0] + "." + cmd_args[1]
-                if key in models.storage.all():
-                    models.storage.all().pop(key)
-                    models.storage.save()
-                else:
-                    print("** no instance found **")
+
+        elif c_name not in my_classes:
+            print("** class doesn't exist **")
+
+        elif i_id == "" or i_id is None:
+            print('** instance id missing **')
+
+        else:
+            key = c_name + "." + i_id
+
+            if key in models.storage.all():
+                del models.storage.all()[key]
+                models.storage.save()
+
             else:
-                print("** instance id missing **")
-        else:
-            print("** class doesn't exist **")
+                print("** no instance found **")
 
-    def do_all(self, arg):
+    def do_all(self, line):
         """
-         Prints all string representation of all
-         instances based or not on the class name
-         """
-        cmd_args = shlex.split(arg)
-        obj_list = []
-        if len(cmd_args) == 0:
-            obj_dict = models.storage.all()
-        elif cmd_args[0] in classes:
-            obj_dict = models.storage.all(classes[cmd_args[0]])
-        else:
-            print("** class doesn't exist **")
-            return False
-        for key in obj_dict:
-            obj_list.append(str(obj_dict[key]))
-        print("[", end="")
-        print(", ".join(obj_list), end="")
-        print("]")
+        Prints all string representation of all instances based
+        or not on the class name. Ex: $ all BaseModel or $ all.
+        The printed result must be a list of strings (like the example below)
+        If the class name doesn’t exist,
+        print ** class doesn't exist ** (ex: $ all MyModel)
+        """
+        if line == "" or line is None:
+            new_list = [str(obj) for key, obj in models.storage.all().items()]
+            print(new_list)
 
-    def do_update(self, arg):
-        """
-         Updates an instance based on the class name and id by adding or
-         updating attribute (save the change into the JSON file)"""
-        args = shlex.split(arg)
-        integers = ["number_rooms", "number_bathrooms", "max_guest",
-                    "price_by_night"]
-        floats = ["latitude", "longitude"]
-        if len(args) == 0:
-            print("** class name missing **")
-        elif args[0] in classes:
-            if len(args) > 1:
-                k = args[0] + "." + args[1]
-                if k in models.storage.all():
-                    if len(args) > 2:
-                        if len(args) > 3:
-                            if args[0] == "Place":
-                                if args[2] in integers:
-                                    try:
-                                        args[3] = int(args[3])
-                                    except ValueError:
-                                        args[3] = 0
-                                elif args[2] in floats:
-                                    try:
-                                        args[3] = float(args[3])
-                                    except ValueError:
-                                        args[3] = 0.0
-                            setattr(models.storage.all()[k], args[2], args[3])
-                            models.storage.all()[k].save()
-                        else:
-                            print("** value missing **")
-                    else:
-                        print("** attribute name missing **")
-                else:
-                    print("** no instance found **")
+        else:
+            c_name = self.parseline(line)[0]
+
+            if c_name not in my_classes:
+                print("** class doesn't exist **")
             else:
-                print("** instance id missing **")
-        else:
+                new_list = [str(value) for key, value in models.storage.all
+                            ().items() if type(value).__name__ == c_name]
+                print(new_list)
+
+    def do_update(self, line):
+        """
+        Updates an instance based on the class name and id by adding
+        or updating attribute (save the change into the JSON file).
+        Ex: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com".
+        Usage: update <class name> <id> <attribute name> "<attribute value>"
+        Only one attribute can be updated at the time
+        You can assume the attribute name is valid (exists for this model)
+        The attribute value must be casted to the attribute type
+        If the class name is missing,
+        print ** class name missing ** (ex: $ update)
+        If the class name doesn’t exist,
+        print ** class doesn't exist ** (ex: $ update MyModel)
+        If the id is missing,
+        print ** instance id missing ** (ex: $ update BaseModel)
+        If the instance of the class name doesn’t exist for the id,
+        print ** no instance found ** (ex: $ update BaseModel 121212)
+        If the attribute name is missing,
+        print ** attribute name missing ** (ex: $ update BaseModel existing-id)
+        If the value for the attribute name doesn’t exist,print
+        ** value missing ** (ex: $ update BaseModel existing-id first_name)
+        All other arguments should not be used (Ex: $ update BaseModel
+        1234-1234-1234 email "aibnb@mail.com" first_name "Betty" = $ update
+        BaseModel 1234-1234-1234 email "aibnb@mail.com")
+        id, created_at and updated_at cant’ be updated.
+        You can assume they won’t be passed in the update command
+        Only “simple” arguments can be updated: string, integer and float.
+        You can assume nobody will try to update list of ids or datetime
+        """
+
+        args = shlex.split(line)
+        num_of_args = len(args)
+        if num_of_args == 0:
+            print('** class name missing **')
+        elif args[0] not in my_classes:
             print("** class doesn't exist **")
-
-    def default(self, arg):
-        """ advanced arguments"""
-        cmds = ["all", "count", "destroy", "show", "update"]
-        n_list = arg.split('.')
-        if len(n_list) > 1:
-            try:
-                instance = n_list[0]
-                command = n_list[1].split('(')[0]
-                if command not in cmds:
-                    print("*** invalid syntax: {} ***".format(arg))
-                if command == cmds[0]:
-                    self.do_all(instance)
-                    return
-                if command == cmds[1]:
-                    print(models.storage.count(instance))
-                    return
-                Id = n_list[1].split('(')[1].split(')')[0]
-                if command == cmds[2]:
-                    Arg = instance + " " + Id
-                    self.do_destroy(Arg)
-                    return
-                if command == cmds[3]:
-                    Arg = instance + " " + Id
-                    self.do_show(Arg)
-                    return
-                if command == cmds[4]:
-                    Arg = (n_list[-1].split(","))
-                    n_id = Arg[0].split("(")[-1]
-                    n_name = Arg[1]
-                    n_value = Arg[2].split(')')[0]
-                    self.do_update("{} \
-{} {} {}".format(instance, n_id, n_name, n_value))
-
-                    return
-            except IndexError:
-                return
-
+        elif num_of_args == 1:
+            print('** instance id missing **')
         else:
-            print("*** invalid syntax: {} ***")
+            key = args[0] + '.' + args[1]
+            instance = models.storage.all().get(key)
+            if instance is None:
+                print('** no instance found **')
+            elif num_of_args == 2:
+                print('** attribute name missing **')
+            elif num_of_args == 3:
+                print('** value missing **')
+            else:
+                if args[3].isdigit():
+                    args[3] = int(args[3])
+                elif args[3].replace('.', '', 1).isdigit():
+                    args[3] = float(args[3])
+
+                setattr(instance, args[2], args[3])
+                setattr(instance, 'updated_at', datetime.now())
+                models.storage.save()
 
 
 if __name__ == '__main__':
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,69 +1,79 @@
 #!/usr/bin/python3
-""" File storage module """
+"""
+File that will be responsible for sereialization
+and deserialization
+"""
+
 import json
 from models.base_model import BaseModel
+from os import path
 from models.user import User
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-import models
 
 
 class FileStorage:
-    """ class FileStorage
-    Serves as file storage engine
-
-    attrs:
-        __file path (str): Name of file path to save object to
-        __objects (dict): Dictionary of instantiated keyword argument
-
     """
+    A class that serializes instances to a JSON file
+    and deserializes JSON file to instances:
+
+    models/engine/file_storage.py
+    Private class attributes:
+    __file_path: string - path to the JSON file (ex: file.json)
+    __objects: dictionary - empty but will store all objects by
+    <class name>.id (ex: to store a BaseModel object with id=12121212,
+    the key will be BaseModel.12121212)
+    Public instance methods:
+    all(self): returns the dictionary __objects
+    new(self, obj): sets in __objects the obj with key <obj class name>.id
+    save(self): serializes __objects to the JSON file (path: __file_path)
+    reload(self): deserializes the JSON file to __objects (only if the JSON
+    file (__file_path) exists ; otherwise, do nothing. If the file doesn’t
+    exist, no exception should be raised)
+    """
+
     __file_path = "file.json"
     __objects = {}
 
-    def all(self, cls=None):
-        """  returns the dictionary __objects """
-        if cls is not None:
-            new_dict = {}
-            for key, value in self.__objects.items():
-                if cls == value.__class__ or cls == value.__class__.__name__:
-                    new_dict[key] = value
-            return new_dict
-        return self.__objects
+    def all(self):
+        """
+        returns the dictionary __objects
+        """
+
+        return (self.__objects)
 
     def new(self, obj):
-        """ sets in __objects the obj with key <obj class name>.id"""
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
+
         key = "{}.{}".format(obj.__class__.__name__, str(obj.id))
-        FileStorage.__objects[key] = obj
+
+        self.__objects[key] = obj
 
     def save(self):
-        """ serializes __objects to the JSON file """
-        to_dict = {}
-        for key, obj in FileStorage.__objects.items():
-            to_dict[key] = obj.to_dict()
+        """
+        serializes __objects to the JSON file (path: __file_path)
+        """
 
-        with open(FileStorage.__file_path, 'w', encoding="UTF-8") as f:
-            json.dump(to_dict, f)
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            dict_json = {key: value.to_dict() for
+                         key, value in FileStorage.__objects.items()}
+            json.dump(dict_json, f)
 
     def reload(self):
-        """ deserializes the JSON file to __objects """
-        try:
-            with open(FileStorage.__file_path, 'r', encoding="utf-8") as f:
-                load = json.load(f)
-                for key, value in load.items():
-                    FileStorage.__objects[key] = eval(
-                        value['__class__'])(**value)
-        except FileNotFoundError:
-            return
+        """
+        deserializes the JSON file to __objects (only if the JSON file
+        (__file_path) exists ; otherwise, do nothing.
+        If the file doesn’t exist, no exception should be raised)
+        """
 
-    def count(self, cls):
-        """
-        counts the number of objects in storage
-        """
-        counter = 0
-        for key in models.storage.all().keys():
-            if cls in key:
-                counter += 1
-        return counter
+        if path.exists(self.__file_path):
+            with open(self.__file_path, mode='r', encoding='utf-8') as f:
+                json_dict = json.load(f)
+            json_dict = {key: eval(value["__class__"])(**value) for
+                         key, value in json_dict.items()}
+            FileStorage.__objects = json_dict
