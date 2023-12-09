fea08778e2b153169cd8b4d96b5cbe0473160d1d
Ayanda Ngubane <ayanda4me@gmail.com>
2023-12-09 03:46:45 -0800

    File storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -51,7 +51,7 @@ class FileStorage:
         sets in __objects the obj with key <obj class name>.id
         """
 
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        key = "{}.{}".format(obj.__class__.__name__, str(obj.id))
 
         self.__objects[key] = obj
 
